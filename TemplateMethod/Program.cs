aa096047dde96f19564dc34c935a45a812126706
Novr_wz05 <Novr_wz05@STHQ0124-15>
2022-11-30 08:51:43 +0400

    new class

--- TemplateMethod/Program.cs
+++ TemplateMethod/Program.cs
@@ -69,13 +69,54 @@ public class SaglamHeyat : HouseBuilder
     }
 }
 
+public class VarliHeyatTerziKompleks : HouseBuilder
+{
+    public override void BuildDam()
+    {
+        Console.WriteLine($"Dam Created");
+    }
+
+    public override void BuildWalls(int wallCount)
+    {
+        Console.WriteLine($"{wallCount} walls created");
+    }
+    public override void BuildWindows()
+    {
+        Console.WriteLine($"Windows created");
+    }
+
+    public void CreatePool()
+    {
+        Console.WriteLine("Pool Created!");
+    }
+
+    public void CreateCleverHomeSystem()
+    {
+        Console.WriteLine("Clever System Added!");
+    }
+
+
+    public VarliHeyatTerziKompleks()
+    {
+        BuildDam();
+        BuildWalls(4);
+        BuildWindows();
+        CreatePool();
+        CreateCleverHomeSystem();
+    }
+}
+
+
 class Program
 {
     static void Main(string[] args)
     {
+        Console.WriteLine("------- Saglam Heyat ------");
         SaglamHeyat house1 = new();
         Console.WriteLine("------- Main Yasayis Kompleksi ------");
         MainYasayisKompleksi house2 = new();
+        Console.WriteLine("------- Varli Heyat Terzi Kompleksi ------");
+        VarliHeyatTerziKompleks house3 = new();
     }
 }
 
